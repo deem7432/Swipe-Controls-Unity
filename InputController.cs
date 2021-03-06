6ebd28b93253993061aea0d3561cb270a21d34b0
Nadeem Shahzad <nadeem.shahzad.10236@gmail.com>
2021-03-06 23:46:34 +0500

    Add files via upload

---
+++ InputController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public static InputController instance;
+
+    private bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+    private Vector2 startTouch, swipeDelta;
+    private bool isDragging = false;
+    private float offset = 125f; // DeadZone Offset
+
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+    public bool Tap { get { return tap; } }
+    public bool SwipLeft { get { return swipeLeft; } }
+    public bool SwipeRight { get { return swipeRight; } }
+    public bool SwipeUp { get { return swipeUp; } }
+    public bool SwipeDown { get { return swipeDown; } }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Update()
+    {
+        tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+
+        #region Standalone
+        if (Input.GetMouseButtonDown(0))
+        {
+            tap = isDragging = true;
+            startTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            Reset();
+        }
+        #endregion
+
+        #region Mobile Controls
+
+        if (Input.touchCount > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                tap = isDragging = true;
+                startTouch = Input.touches[0].position;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDragging = false;
+                Reset();
+            }
+        }
+        #endregion
+
+        #region Distance Calculations
+        swipeDelta = Vector2.zero;
+        if (isDragging)
+        {
+            if (Input.touchCount > 0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }
+        }
+        #endregion
+
+        #region Direction Define
+        if (swipeDelta.magnitude > offset)
+        {
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                if (x > 0)
+                    swipeRight = true;
+                else
+                    swipeLeft = true;
+            }
+            else
+            {
+                if (y > 0)
+                    swipeUp = true;
+                else
+                    swipeDown = true;
+            }
+            Reset();
+        }
+        #endregion
+    }
+
+    private void Reset()
+    {
+        swipeDelta = startTouch = Vector2.zero;
+        isDragging = false;
+    }
+}
